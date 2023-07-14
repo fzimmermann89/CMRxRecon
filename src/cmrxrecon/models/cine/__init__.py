af6c9df00a9e925c27b632999e0eefd4f88e55fc
Felix Zimmermann <fzimmermann89@gmail.com>
2023-07-14 13:39:30 +0200

    add models to init

--- src/cmrxrecon/models/cine/__init__.py
+++ src/cmrxrecon/models/cine/__init__.py
@@ -1,3 +1,7 @@
 from .base import CineModel
 from .basicunet import BasicUNet
 from .csm_free_recon import CSMFreeReconNN
+from .joint_csm_img_unet import JointCSMImageReconNN
+from .nn_pdhg_plus_csms import NNPDHG4DynMRIwTVRecon
+from .cdl_fista import ConvDicoLearnFISTA
+from .rss import RSS
