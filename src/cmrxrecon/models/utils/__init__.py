16e5bcd805b694441f7f1f55a265ecc73128b3fb
Felix Zimmermann <fzimmermann89@gmail.com>
2023-08-01 18:35:21 +0200

    helper functions for rss

--- src/cmrxrecon/models/utils/__init__.py
+++ src/cmrxrecon/models/utils/__init__.py
@@ -2,5 +2,15 @@ import torch
 
 
 @torch.jit.script
-def rss(x: torch.Tensor) -> torch.Tensor:
-    return x.abs().square().sum(1).sqrt()
+def rss(x: torch.Tensor, dim: int = 1) -> torch.Tensor:
+    return (x.real.square() + x.imag.square()).sum(dim).sqrt()
+
+
+@torch.jit.script
+def reciprocal_rss(x: torch.Tensor, dim: int = 1) -> torch.Tensor:
+    return (x.real.square() + x.imag.square()).sum(dim).rsqrt()
+
+
+@torch.jit.script
+def masked_mean(x: torch.Tensor, mask: torch.Tensor, dim: tuple[int, ...] = (-1,), keepdim: bool = False) -> torch.Tensor:
+    return (x * mask).sum(dim=dim, keepdim=keepdim) / mask.sum(dim=dim, keepdim=keepdim)
