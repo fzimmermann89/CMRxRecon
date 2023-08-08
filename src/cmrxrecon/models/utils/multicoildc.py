1601a1f4b3889363f36f683d1f5826d8bb133584
Felix Zimmermann <fzimmermann89@gmail.com>
2023-08-08 15:56:15 +0200

    dc: allow k space input/output

--- src/cmrxrecon/models/utils/multicoildc.py
+++ src/cmrxrecon/models/utils/multicoildc.py
@@ -2,7 +2,7 @@ import torch
 
 
 class MultiCoilDCLayer(torch.nn.Module):
-    def __init__(self, Nc: int, embed_dim: int = 0, lambda_bias: float = 1.0):
+    def __init__(self, Nc: int, embed_dim: int = 0, lambda_bias: float = 1.0, return_k: bool = False, input_nn_k: bool = False):
         """
         Data-consistency layer for multiple receiver coils
         computes the argmin of min_x 1/2|| F_I x - y||_2**2 + lambda/2|| x - xnn||_2**2
@@ -14,9 +14,12 @@ class MultiCoilDCLayer(torch.nn.Module):
         Parameters
         ----------
         Nc: Number of coils
-        ebmed_dim:
+        embed_dim:
             Embedding dimension for the lambda conditioning.
             If 0, static lambdas for each coil are used.
+        lambda_bias: Initial bias for the lambda conditioning
+        return_k: If True, return k-space data, otherwise return image space data
+        input_nn_k: If True, the NN input is assumed to be k-space data, otherwise image space data.
         """
         super().__init__()
         if embed_dim > 0:
@@ -26,13 +29,15 @@ class MultiCoilDCLayer(torch.nn.Module):
                 self.lambda_proj.bias[:] = lambda_bias
         else:
             self.lambda_proj = lambda x: x + lambda_bias
+        self.input_nn_k = input_nn_k
+        self.return_k = return_k
 
-    def forward(self, k: torch.Tensor, xnn: torch.Tensor, mask: torch.Tensor, lambda_embed: torch.Tensor) -> torch.Tensor:
+    def forward(self, k: torch.Tensor, nn: torch.Tensor, mask: torch.Tensor, lambda_embed: torch.Tensor) -> torch.Tensor:
+        knn = nn if self.input_nn_k else torch.fft.fft2(nn, norm="ortho")
         lam = self.lambda_proj(lambda_embed)[:, :, None, None, None, None]  # [Nb, Nc, 1, 1, 1, 1]
         mask = mask.unsqueeze(1)  # [Nb, Nc=1, Nz=1, Nz=1, Nu, Nf=1]
         fk = mask * (1.0 / (1.0 + lam))  # facor for k data, 0 for missing data
-        fn = 1 - fk  # factor for xnn data
-        knn = torch.fft.fft2(xnn, norm="ortho")
-        xreg = fn * knn + fk * k
-        xdc = torch.fft.ifft2(xreg, norm="ortho")
-        return xdc
+        fn = 1 - fk  # factor for knn data
+        kdc = fn * knn + fk * k
+        ret = kdc if self.return_k else torch.fft.ifft2(kdc, norm="ortho")
+        return ret
