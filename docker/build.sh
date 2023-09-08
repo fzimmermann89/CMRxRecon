ba61a31f935fc5ffd1dd0ad1652a9e7d996d953d
Felix Zimmermann <fzimmermann89@gmail.com>
2023-09-08 12:21:11 +0200

    docker update

--- docker/build.sh
+++ docker/build.sh
@@ -2,4 +2,4 @@
 docker build --build-arg HTTP_PROXY=$http_proxy \
 --build-arg HTTPS_PROXY=$http_proxy --build-arg NO_PROXY="$no_proxy" \
 --build-arg http_proxy=$http_proxy --build-arg https_proxy=$http_proxy \
---build-arg no_proxy="$no_proxy" .. -t cmrxrecon-jabber -f Dockerfile
+--build-arg no_proxy="$no_proxy" .. -t docker.synapse.org/syn51730458/cine:v169a -f Dockerfile
